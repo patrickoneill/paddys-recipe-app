8e99fe40c6d445a501c6b5a390b49fc390851c2c
Patrick O Neill <patrickonei@gmail.com>
2019-01-22 13:51:22 +0000

    added basic app functions and linked in the database

---
+++ app.py
@@ -0,0 +1,40 @@
+import os
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+app.secret_key = "some_secret"
+
+app.config["MONGO_DBNAME"] = "recipes-app"
+app.config["MONGO_URI"] = "mongodb://admin:dragon99@ds159387.mlab.com:59387/recipes-app"
+
+mongo = PyMongo(app)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html',
+    )
+    
+@app.route('/see_recipes')
+def see_recipes():
+    return render_template('recipes.html', 
+    recipeCollection=mongo.db.recipeCollection.find(),
+    style=mongo.db.style.find())
+    
+@app.route('/create_recipe')
+def create_recipe():
+    return render_template('create-recipe.html')
+    
+@app.route('/edit_recpies')
+def edit_recipe():
+    return render_template('edit-recipe.html')
+    
+    
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
+            
