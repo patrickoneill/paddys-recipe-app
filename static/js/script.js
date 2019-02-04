acc2c2b2fc91840f8f6cddd3456abd413c8bc6ac
Patrick O Neill <patrickonei@gmail.com>
2019-02-04 11:32:54 +0000

    added JS file for all the queries

---
+++ static/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('.sidenav').sidenav();
+});
+$(document).ready(function() {
+    M.updateTextFields();
+});
+$(document).ready(function() {
+    $('select').formSelect();
+});
+$(document).ready(function() {
+    $('.collapsible').collapsible();
+});
+$(document).ready(function() {
+    $('select').formSelect();
+});
+
