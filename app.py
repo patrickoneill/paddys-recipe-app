3b83b37729f7b812a6ab9d4c3d6ee766a6061640
Patrick O Neill <patrickonei@gmail.com>
2019-04-09 20:29:57 +0000

    small changes and some code that isnt linked up yet

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect, url_for, request, session, escape, flash
+from flask import Flask, render_template, redirect, url_for, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -17,39 +17,42 @@ def index():
 @app.route('/see_recipes')
 def see_recipes():
     return render_template('recipes.html', 
-    recipeCollection=mongo.db.recipeCollection.find())
+    recipeCollection=mongo.db.recipeCollection.find(),
+    recipeCat = mongo.db.recipeCat.find())
 
 @app.route('/create_recipe', methods = ['GET','POST'])
 def create_recipe():
     return render_template('createRecipe.html',
     recipeCat = mongo.db.recipeCat.find(),
+    recipeMeal = mongo.db.recipeMeal.find(),
     recipeAllergy = mongo.db.recipeAllergy.find())
 
-@app.route('/add_recipe', methods=['GET', 'POST'])
+@app.route('/add_recipe', methods=['GET','POST'])
 def add_recipe():
     if request.method == 'POST':
         recipeCollection = mongo.db.recipeCollection
-        ingredients = request.form['recipe_ingredients']
-        cooking = request.form['recipe_cooking']
         recipeCollection.insert({
             'recipe_name': request.form['recipe_name'],
             'recipe_title': request.form['recipe_title'],
             'recipe_style': request.form['recipe_style'],
-            'recipe_ingredients': ingredients,
+            'recipe_ingredients': request.form['recipe_ingredients'],
             'dish_allergy': request.form.getlist('dish_allergy'),
-            'recipe_cooking': cooking,
+            'recipe_cooking': request.form['recipe_cooking'],
             'recipe_serving': request.form['recipe_serving'],
-            'recipe_time': request.form['recipe_time'],
+            'recipe_time_hr': request.form['recipe_time_hr'],
+            'recipe_time_min': request.form['recipe_time_min'],
             'meat_free': request.form['meat_free'],
+            'recipe_score': request.form['recipe_score'],
             'recipe_user': request.form['recipe_user'],
         })
     return redirect(url_for('see_recipes' ))
     
 @app.route('/edit_recipe/<task_id>')
 def edit_recipe(task_id):
-    the_recipe = mongo.db.recipeCollection.find_one({'_id': ObjectId(task_id)})
-    all_styles = mongo.db.style.find()
-    return render_template('editRecipe.html', task=the_recipe, categories=all_styles)
+    recipeCollection = mongo.db.recipeCollection.find_one({'_id': ObjectId(task_id)})
+    recipeCat = mongo.db.recipeCat.find()
+    recipeAllergy = mongo.db.recipeAllergy.find()
+    return render_template('editRecipe.html', task=recipeCollection, categories=recipeCat, allergy=recipeAllergy)
     
 @app.route('/that_recipe/<task_id>')
 def that_recipe(task_id):
@@ -60,16 +63,47 @@ def that_recipe(task_id):
 def update_task(task_id):
     if request.method == 'POST':
         recipeCollection = mongo.db.recipeCollection
-        recipeCollection.update( {'_id': ObjectId(task_id)},
-        {
-            'recipe_name':request.form.get['recipe_name'],
-            'recipe_title':request.form.get['recipe_title'],
+        recipeCollection.insert({
+            'recipe_name': request.form['recipe_name'],
+            'recipe_title': request.form['recipe_title'],
             'recipe_style': request.form['recipe_style'],
-            'recipe_ingredients':request.form.get['recipe_ingredients'],
-            'recipe_cooking': request.form.get['recipe_cooking'],
-            'recipe_user': request.form.get['recipe_user'],
+            'recipe_ingredients': request.form['recipe_ingredients'],
+            'dish_allergy': request.form.getlist('dish_allergy'),
+            'recipe_cooking': request.form['recipe_cooking'],
+            'recipe_serving': request.form['recipe_serving'],
+            'recipe_time_hr': request.form['recipe_time_hr'],
+            'recipe_time_min': request.form['recipe_time_min'],
+            'meat_free': request.form['meat_free'],
+            'recipe_score': request.form['recipe_score'],
+            'recipe_user': request.form['recipe_user'],
         })
     return redirect(url_for('see_recipes'))
+    
+@app.route('/recipeCat_add', methods=['POST'])
+def recipeCat_add():
+    recipeCat_add = mongo.db.recipeCat
+    recipeCat_add_doc = {'recipe_style': request.form['recipeCat_add']}
+    recipeCat_add.insert_one(recipeCat_add_doc)
+    return redirect(url_for('see_recipes'))
+    
+# @app.route('/category/<meat_free>')
+# def category(meat_free):
+   
+#     if meat_free == 'Vegetarian':
+#         the_category = mongo.db.recipeCollection.find({"vegetarian": "Vegetarian"})
+#     elif category_name == 'Vegan':
+#         the_category = mongo.db.recipes.find({"vegetarian": "Vegan"})
+#     else:
+#         the_category = mongo.db.recipes.find({"category": category_name})
+#     return render_template('category.html', category_name=category_name, category=the_category, picture_number=pic, all_categories=all_categories)
+
+app.route('/recipe_score/<add_id>')
+def recipe_score(add_id):
+    recipeCollection = mongo.db.recipeCollection.find_one({"_id": ObjectId(add_id)})
+    score = recipeCollection["recipe_score"]
+    score += 1
+    mongo.db.recipeCollection.update({'_id': ObjectId(add_id)}, { '$set': { "recipe_score": score }})
+    return redirect(url_for('see_recipes', add_id=add_id))
     
 @app.route('/delete_recipe/<task_id>')
 def delete_recipe(task_id):
