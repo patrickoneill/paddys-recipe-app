67e4e79586fba4a4d107e9d944e2320881f3512f
Patrick O Neill <patrickonei@gmail.com>
2019-05-28 18:33:20 +0000

    changed debug to false

--- app.py
+++ app.py
@@ -104,5 +104,5 @@ def delete_recipe(task_id):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
             
